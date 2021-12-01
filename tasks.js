3cc84ca598fe52aa20fbb49028b004671f0779ab
mal-19 <95008515+mal-19@users.noreply.github.com>
2021-12-01 16:21:46 +0100

    Assignment done

--- tasks.js
+++ tasks.js
@@ -1,4 +1,4 @@
 // Tell the user to enter two numbers then show the multiply the numbers and show the result
 var number1 = 50;
 var number2 = 75;
-var result = number1 * number2
+var result = number1 * number2;
