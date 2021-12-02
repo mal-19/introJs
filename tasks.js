44cf3da09076849605d9ea7fc86178c270d35620
mal-19 <95008515+mal-19@users.noreply.github.com>
2021-12-03 00:54:27 +0100

    2nd task

--- tasks.js
+++ tasks.js
@@ -1,1 +1,10 @@
 // Tell the user to enter two numbers then show the multiply the numbers and show the result
+var ftHeight = prompt ("Enter the height in (ft)");
+// convert to number
+ftHeight = parseFloat("fHeight");
+var inHeight = ftHeight * 12;
+//convert to cm
+var cmHeight = inHeight * 2.54;
+result =inHeight * 2.54;
+alert('The height in cm is', + cmHeight.toFixed(2) );
+
