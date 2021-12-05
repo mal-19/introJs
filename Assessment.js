c35272c9ae259a32a2a54862c4e95904e90dc9a9
mal-19 <95008515+mal-19@users.noreply.github.com>
2021-12-05 22:25:46 +0100

    assignmentish

---
+++ Assessment.js
@@ -0,0 +1,9 @@
+var cashInHand=1300;
+var monster = 500
+var item7withchicken = 800
+var item7mixwithextrarice = 1000
+var Item7withbeef = 600
+var EngineBurgerBreadandAkara  =250
+var SoftDrinks =150
+var Ewagoinwithbread =450
+var Fearless = 250
