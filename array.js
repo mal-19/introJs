bcf0581af255c1d5ab6939199eb69a7f3c1889e6
mal-19 <95008515+mal-19@users.noreply.github.com>
2021-12-09 21:27:24 +0100

    array assessment

---
+++ array.js
@@ -0,0 +1,12 @@
+// create an array of number 
+var numbers= [6,7,9,8,3,34,56,86,12,32];
+// remove the last item of the array
+numbers.pop();
+// remove the first item of the array
+numbers.shift();
+// add my name to the array 
+numbers.push(Miracle);
+// add the current hour to the array
+var today= new Date();
+numbers.push(new Date());
+numbers.splice(8,1)
