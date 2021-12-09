a567b506e64a2d26a131d530c46e72e7ba1eee68
mal-19 <95008515+mal-19@users.noreply.github.com>
2021-12-09 23:13:24 +0100

    updated version

--- array.js
+++ array.js
@@ -1,12 +1,23 @@
 // create an array of number 
 var numbers= [6,7,9,8,3,34,56,86,12,32];
 // remove the last item of the array
+console.log(numbers,"initial number")
+
 numbers.pop();
+console.log(numbers,"after removing the last item")
 // remove the first item of the array
 numbers.shift();
-// add my name to the array 
-numbers.push(Miracle);
+console.log(numbers,"after removing the first item")
+
+// add my name to the array
+var name="Miracle" ;
+numbers.push(name);
+console.log(numbers,"after pushing the name to the last index")
+
 // add the current hour to the array
-var today= new Date();
-numbers.push(new Date());
-numbers.splice(8,1)
+var today= new Date().getHours();
+numbers.push(today);
+console.log(numbers,"after adding current hour")
+
+numbers.splice(4,1);
+console.log(numbers,"after removing the 4th element")
